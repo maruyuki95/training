453ed48f11ff674f5b5585b2e6f4dd55c5849000
Yuki Maruyama <mymelon.95@gmail.com>
2018-01-01 00:53:43 +0900

    modify ()

--- training/src/training/Main20171231.java
+++ training/src/training/Main20171231.java
@@ -3,68 +3,49 @@ package training;
 public class Main20171231 {
 
 	public static void main(String[] args) {
-		String msg = "((((((()))))))(()())";
-		int length =20;
-		int num = 5;
+		String msg = "(())";
+		int length = 4;
+		int num = 4;
 
 		int ret = new Main20171231().execute(msg, length, num);
 		System.out.println(ret);
 	}
 
 	public int execute(String msg, int length, int num) {
-		boolean left = isLeft(msg, num);
-
+		Parenthesis parenthesis = initParenthesis(msg, num);
+		
 		int depth = 0;
 		for (int i = 1; i <= length; i++) {
-			if ((left && i < num) || (!left && num < i)) {
+			if (parenthesis.needToCheck(i, num) == false) {
 				continue;
 			}
 
-			String str = find(msg, i, left);
+			String str = parenthesis.find(i);
 			if (isLeft(str)) {
 				depth++;
 			} else {
 				depth--;
 			}
-			
-			if(depth == 0) {
-				if(left) {
-					return i;
-				}else {
-					return msg.length() - i + 1;
-				}
+
+			if(depth != 0) {
+				continue;
 			}
+
+			return parenthesis.getResult(i);
 		}
 
 		System.out.println("見つからんかった...orz");
 		return 0;
 	}
 
-	/**
-	 * @param msg
-	 * @param i
-	 * @param left 左から数える場合はtrue
-	 * @return　msg の i 番目の文字を返す。
-	 */
-	private String find(String msg, int i, boolean left) {
-		int index;
-		if(left) {
-			index = i - 1;
+	private Parenthesis initParenthesis(String msg, int num) {
+		String targetChar = String.valueOf(msg.charAt(num - 1));
+
+		if(isLeft(targetChar)) {
+			return new LeftParenthesis(msg);
 		}else {
-			index = msg.length() - i;
+			return new RightParenthesis(msg);
 		}
-
-		return String.valueOf(msg.charAt(index));
-	}
-
-	/**
-	 * @param msg
-	 * @param num
-	 * @return msg の左から num 番目の文字が「(」だったら、trueを返す。
-	 */
-	private boolean isLeft(String msg, int num) {
-		String str = find(msg, num, true);
-		return isLeft(str);
 	}
 
 	/**
@@ -75,4 +56,59 @@ public class Main20171231 {
 		return str != null && str.equals("(");
 	}
 
+	private abstract class Parenthesis {
+		protected String msg;
+		public Parenthesis(String msg) {
+			this.msg = msg;
+		}
+		public abstract String find(int index);
+		public abstract boolean needToCheck(int index, int num);
+		public abstract int getResult(int index);
+	}
+
+	private class RightParenthesis extends Parenthesis {
+		public RightParenthesis(String msg) {
+			super(msg);
+		}
+
+		@Override
+		public String find(int index) {
+			int i = msg.length() - index;
+			return String.valueOf(msg.charAt(i));
+		}
+
+		@Override
+		public boolean needToCheck(int index, int num) {
+			return index <= num;
+		}
+
+		@Override
+		public int getResult(int index) {
+			return msg.length() - index + 1;
+		}
+
+	}
+	
+	private class LeftParenthesis extends Parenthesis {
+		public LeftParenthesis(String msg) {
+			super(msg);
+		}
+
+		@Override
+		public String find(int index) {
+			int i = index - 1;
+			return String.valueOf(msg.charAt(i));
+		}
+
+		@Override
+		public boolean needToCheck(int index, int num) {
+			return num <= index;
+		}
+
+		@Override
+		public int getResult(int index) {
+			return index;
+		}
+
+	}
 }
