b72ae529677d8fcbc2f838cb788155aae3017209
Yuki Maruyama <mymelon.95@gmail.com>
2020-07-05 22:02:54 +0900

    1st commit about no.4
    challenge08.txt -> TLE

--- training/src/training/yukicoder/no4/java/Main.java
+++ training/src/training/yukicoder/no4/java/Main.java
@@ -1,5 +1,8 @@
 package training.yukicoder.no4.java;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -13,16 +16,64 @@ public class Main {
 		outputResult(canDevideInHalf(weights));
 	}
 
-	private boolean canDevideInHalf(int[] weights) {
+	boolean canDevideInHalf(int[] weights) {
 		int totalWeight = total(weights);
 		if (totalWeight % 2 != 0) {
 			return false;
 		}
 
 		int halfTotalWeight = totalWeight /2 ;
-		return ;
+		startTime = System.currentTimeMillis();
+		return canMakeWeight(convertToSortedList(weights), halfTotalWeight);
 	}
 
+	private long startTime = 0;
+	private long removeTime = 0;
+	private boolean canMakeWeight(List<Integer> weights, int goalWeight) {
+		List<Integer> copyList = new ArrayList<>(weights);
+		
+		for (Integer weight : weights) {
+			if (weight == goalWeight) {
+				return true;
+			} 
+			
+			int sum = summary(copyList);
+			if (sum < goalWeight) {
+				return false;
+			}
+			
+			long removeStart = System.currentTimeMillis();
+			copyList.remove(weight);
+			removeTime += System.currentTimeMillis() - removeStart;
+			if (weight > goalWeight) {
+				continue;
+			}
+			
+			if (canMakeWeight(copyList, goalWeight - weight)) {
+				return true;
+			}
+		}
+		System.out.println(100 * removeTime / (System.currentTimeMillis() - startTime));
+		return false;
+	}
+
+	private int summary(List<Integer> list) {
+		int sum = 0;
+		for (Integer integer : list) {
+			sum += integer;
+		}
+		return sum;
+	}
+
+	private List<Integer> convertToSortedList(int[] array) {
+		List<Integer> ret = new ArrayList<>();
+		for (int i : array) {
+			ret.add(i);
+		}
+		Collections.sort(ret);
+		return ret ;
+	}
+	
 	private int total(int[] values) {
 		int totalValue = 0;
 		for (int value : values) {
---
+++ training/src/training/yukicoder/no4/java/MainTest.java
@@ -0,0 +1,45 @@
+package training.yukicoder.no4.java;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MainTest extends Main {
+
+	@Test
+	public void test01() {
+		int[] weights = {1, 2, 3};
+		assertTrue(canDevideInHalf(weights));
+	}
+
+	@Test
+	public void test02() {
+		int[] weights = {1, 2, 3, 4, 5};
+		assertFalse(canDevideInHalf(weights));
+	}
+
+	@Test
+	public void test03() {
+		int[] weights = {62, 8, 90, 2, 24, 62, 38, 64, 76, 60, 30, 76, 80, 74, 72};
+		assertFalse(canDevideInHalf(weights));
+	}
+
+	@Test
+	public void testMany() {
+		int[] weights = {
+				16, 100, 48, 18, 62, 46, 90, 58, 48, 20,
+				82, 32, 86, 86, 76, 8, 56, 60, 82, 26, 
+				8, 40, 20, 16, 22, 68, 100, 58, 34, 72,
+				96, 54, 26, 36, 22, 68, 60, 100, 60, 44,
+				36, 46, 88, 74, 72, 92, 58, 84, 90, 16, 
+				4, 8, 18, 90, 38, 82, 50, 94, 62, 8, 
+				46, 34, 100, 50, 4, 8, 56, 18, 58, 68,
+				98, 64, 72, 28, 32, 30, 28, 74, 52, 40,
+				32, 90, 60, 4, 36, 92, 12, 10, 4, 98, 78, 
+				48, 12, 66, 6, 34, 34, 76, 78, 90
+};
+		assertFalse(canDevideInHalf(weights));
+	}
+}
+
